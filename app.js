3af59dda445b6452fcfe9a63e9aaa55b4d3b1f3f
tobyaskew1 <46008253+tobyaskew1@users.noreply.github.com>
2024-05-02 18:40:48 +0100

    Changing the colour scheme of the site!

---
+++ app.js
@@ -0,0 +1,21 @@
+var countDownDate = new Date("May 31, 2025 10:00:00").getTime();
+
+var x = setInterval(function() {
+
+  var now = new Date().getTime();
+
+  var distance = countDownDate - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("countdown").innerHTML = days + " days, " + hours + " hours, "
+  + minutes + " minutes, " + seconds + " seconds ";
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "It's our wedding day!";
+  }
+}, 1000);
